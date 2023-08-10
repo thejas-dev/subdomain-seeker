2bca6a48f584ef7b406a0f00809add6c5b667784
thejas-dev <thejasgamerx123@gmail.com>
2023-08-11 00:13:59 +0530

    make itbtett

--- api/index.go
+++ api/index.go
@@ -34,7 +34,6 @@ var addr = flag.String("addr", ":8080", "http service address")
 var upgrader = websocket.Upgrader{} // use default options
 
 func runTheTool(c *websocket.Conn, mt int, message []byte, ctx context.Context) {
-
 	file, err := os.Open("wordlists.txt")
 	if err != nil {
 		fmt.Println("Error opening file", err)
